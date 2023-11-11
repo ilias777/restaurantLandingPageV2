22fa0c462b1719b6ea332de36b3dbb7c5829fe1b
Ilias <dougias.ilias@gmail.com>
2023-11-11 15:07:43 +0200

    Add functionality on main.js

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
 import "./style.scss";
+
+// Menu Button
+const menuBtn = document.querySelector(".menu-box");
+const list = document.querySelector(".list");
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    list.classList.add("list-open");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    list.classList.remove("list-open");
+    menuOpen = false;
+  }
+});
+
+list.addEventListener("click", () => {
+  menuBtn.classList.remove("open");
+  list.classList.remove("list-open");
+  menuOpen = false;
+});
+
+// Accordion
+const acc = document.querySelectorAll(".accordion").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    let panel = btn.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+});
